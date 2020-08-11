7dc64e4bb9ac478420bdd405ab5e9a815713e4de
baohui <aaabaohui@gmail.com>
2020-08-11 17:22:39 +0800

    Init Project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   validates :name, presence: true, 
     length: { maximum: Settings.validations.name.max_length }
 
@@ -20,12 +21,30 @@ class User < ApplicationRecord
     email.downcase! 
   end
 
-  def User.digest string
-    cost =  if ActiveModel::SecurePassword.min_cost
-              BCrypt::Engine::MIN_COST
-            else
-              BCrypt::Engine.cost
-            end
-    BCrypt::Password.create string, cost: cost
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+             BCrypt::Password.create(string, cost: cost)
+    end
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
